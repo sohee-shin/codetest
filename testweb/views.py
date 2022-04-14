b4fef0598505d1d71b5d8b2c671dd61bb783e116
sohee <herasi14@naver.com>
2022-04-14 16:05:25 +0900

    새로운 테스트 웹 페이지 생성 완료

---
+++ testweb/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from rest_framework.views import APIView
+
+
+class Main(APIView):
+    def get(self,request):
+        return render(request,"testweb/main.html")
+
+
